6068ad4c93dc64a93711a8598f1de7e84c94930a
Rod Johnson <johnsonroda@gmail.com>
2019-01-17 14:47:34 -0800

    Initial commit from Atomist

---
+++ src/main/java/con/Frog2Application.java
@@ -0,0 +1,12 @@
+package con;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Frog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Frog2Application.class, args);
+	}
+}
